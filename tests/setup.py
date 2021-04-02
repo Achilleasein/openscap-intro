305a1d3c6bd777334fced4caee6bc176a621041b
achilleasein <achilleasein@gmail.com>
2021-04-02 19:13:40 -0400

    installing scanner

--- tests/setup.py
+++ tests/setup.py
@@ -1,16 +1,23 @@
 import os
+import subprocess
 
-executable_commands = [ 'sudo dnf install openscap-scanner',
-                        'sudo dnf install scap-security-guide'
+executable_commands = [ "sudo dnf install openscap-scanner",
+                        "sudo dnf install scap-security-guide"
         ]
 
 def installopenscap():
     print('Beginning installing openscap-scaner')
-    subprocess.
+    print(executable_commands[0])
+    out = subprocess.run(['sudo', 'dnf', 'install', 'openscap-scanner'], stdout=subprocess.PIPE)
+    out.stdout.decode('utf-8')
+    if out.stderr != None:
+        return 'Error'
 
 def main():
     print('Beginning setting up...')
-
+    err = installopenscap()
+    if err != None:
+        return 1
 
 if __name__ == '__main__':
     main()
